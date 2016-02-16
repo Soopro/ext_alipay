68a34aa48f5508e640124ebaedfd25b5c38f492c
majik <mumu296979387@gmail.com>
2016-02-16 21:10:36 +0800

    xxx

--- server/blueprints/payment/main.py
+++ server/blueprints/payment/main.py
@@ -1,45 +1,33 @@
-#coding=utf-8
+# coding=utf-8
 from __future__ import absolute_import
 
+from .routes import urls
 from flask import Blueprint, request, current_app
-from .models import AlipayProfile, AlipayOrder
-from .routes import urlpatterns
 from errors.base_errors import APIError
-from utils.base_utils import make_json_response, route_inject
-from utils.request import verify_token
+from utils.verify import verify_outer, verify_token
+from utils.base_utils import route_inject, make_json_response
 
-
-bp_name = 'alipay'
+bp_name = 'payment'
 
 open_api_endpoints = [
-    "{}.get_shoptitle".format(bp_name),
-    "{}.new_order".format(bp_name),
-    "{}.get_member_id_order".format(bp_name),
-    "{}.get_order".format(bp_name),
-    "{}.confirm_payment".format(bp_name),
-    "{}.log_payment".format(bp_name)
+    
 ]
 
+blueprint = Blueprint(bp_name, __name__)
 
-blueprint = Blueprint(bp_name, __name__, static_folder='static')
-
-route_inject(blueprint, urlpatterns)
-
-model_list = [AlipayProfile, AlipayOrder]
+route_inject(blueprint, urls)
 
 
 @blueprint.before_app_first_request
 def before_first_request():
-    current_app.mongodb_database.register(model_list)
-    return
+    from .terms import *
+    current_app.mongodb_database.register()
 
 
 @blueprint.before_request
 def before_request():
-    if request.endpoint in open_api_endpoints:
-        return
-    verify_token()
-    return
+    if request.endpoint not in open_api_endpoints:
+        verify_token()
 
 
 @blueprint.errorhandler(APIError)
